7baa22b6945d6bd4c9e5d846a745b902b92aa7db
ruben056 <demuynck_ruben@hotmail.com>
2017-12-11 12:05:12 +0100

    Added thymeleaf template engine and pass variables to template from server and from client/browser

--- src/main/java/be/rd/msmvc/ctrl/hello/HelloCtrl.java
+++ src/main/java/be/rd/msmvc/ctrl/hello/HelloCtrl.java
@@ -1,16 +1,33 @@
 package be.rd.msmvc.ctrl.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloCtrl {
 
-	@RequestMapping(path = "/", method = RequestMethod.GET)
+	@RequestMapping(path = "/rest/hello", method = RequestMethod.GET)
 	@ResponseBody
 	public String hello() {
 		return "Hello there ...!" + System.lineSeparator();
+	}
+	
+	@RequestMapping(path = "/mvc")
+	public String resultPage(Model model) {	
+		model.addAttribute("msg", "This msg is passed to the view from the ctrl");
+		return "resultPage";
+	}
+	
+	@RequestMapping(path = "/mvc/hello")
+	public String helloWithTemplate(@RequestParam("name")String name ,Model model) {
+		if(name == null || name.length()==0) {
+			name = "Default name";
+		}
+		model.addAttribute("msg", "hello " + name);
+		return "resultPage";
 	}
 }
