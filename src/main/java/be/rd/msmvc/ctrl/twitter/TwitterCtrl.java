153fcb6a956000e04bc8d5c2b498cff3ea9ceb8d
ruben056 <demuynck_ruben@hotmail.com>
2017-12-11 13:26:18 +0100

    Added serverside check on userinput and redirect depending on result of check using flashattributes and normal redirect attributes to pass arguments to redirect url

--- src/main/java/be/rd/msmvc/ctrl/twitter/TwitterCtrl.java
+++ src/main/java/be/rd/msmvc/ctrl/twitter/TwitterCtrl.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -11,33 +13,53 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/mvc/twitter")
 public class TwitterCtrl {
 
 	@Autowired
 	private Twitter twitter;
-	
-	@RequestMapping("/mvc/twitter")
-	public String searchSomething(@RequestParam(defaultValue="snow") String search, Model model) {
+
+	@RequestMapping("/")
+	public String home() {
+		return "twitter/searchPage";
+	}
+
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
 		
+		if (search.toLowerCase().contains("struts")) {
+			redirectAttributes.addFlashAttribute("error", "Try using spring instead!");
+			return "redirect:/mvc/twitter";
+		}	
+		else {
+			redirectAttributes.addAttribute("search", search);
+			return "redirect:/mvc/twitter/result";
+		}
+	}
+
+	@RequestMapping("/result")
+	public String searchSomething(@RequestParam(defaultValue = "snow") String search, Model model) {
+
 		SearchResults searchResults = twitter.searchOperations().search(search);
 		List<Tweet> results = getFirstResults(searchResults, 10);
-		
+
 		model.addAttribute("tweets", results);
 		model.addAttribute("search", search);
-		return "tweetsPage";
+		return "twitter/tweetsPage";
 	}
 
 	private List<Tweet> getFirstResults(SearchResults searchResults, int nrOfResults) {
-		
-		if(searchResults.getTweets().isEmpty()) {
+
+		if (searchResults.getTweets().isEmpty()) {
 			return Collections.<Tweet>emptyList();
 		}
-		
-		return searchResults.getTweets().stream()
-		 		.limit(nrOfResults)
-		 		.collect(Collectors.toList());
+
+		return searchResults.getTweets().stream().limit(nrOfResults).collect(Collectors.toList());
 	}
 }
