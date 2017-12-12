d92f10a69e65614703c00d43c3a6fdaed06a8389
ruben056 <demuynck_ruben@hotmail.com>
2017-12-12 13:16:42 +0100

    * Added i18n to validation messages * Added way to change local/language using ** sessionLocalResolver and tell it tolisten to requestparam "lang" ** on page: dropdown menu with to links to change locale using ?lang=fr or ?lang=en etc..

--- src/main/java/be/rd/msmvc/config/WebConfiguration.java
+++ src/main/java/be/rd/msmvc/config/WebConfiguration.java
@@ -2,9 +2,14 @@ package be.rd.msmvc.config;
 
 import java.time.LocalDate;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import be.rd.msmvc.config.formatter.USLocalDateTimeFormatter;
 
@@ -14,5 +19,22 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addFormatterForFieldType(LocalDate.class, new USLocalDateTimeFormatter());
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new SessionLocaleResolver();
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
