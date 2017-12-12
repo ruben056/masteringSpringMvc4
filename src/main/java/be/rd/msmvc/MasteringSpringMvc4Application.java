247a7a731953d5b1ce8293a088b28493779bdb38
ruben056 <demuynck_ruben@hotmail.com>
2017-12-12 20:11:57 +0100

    Create way to pass image to the client Move some paths to configurationproperties using spring @ConfigurationProperties

--- src/main/java/be/rd/msmvc/MasteringSpringMvc4Application.java
+++ src/main/java/be/rd/msmvc/MasteringSpringMvc4Application.java
@@ -2,7 +2,11 @@ package be.rd.msmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import be.rd.msmvc.config.upload.PicturesUploadProperties;
+
+@EnableConfigurationProperties(PicturesUploadProperties.class)
 @SpringBootApplication
 public class MasteringSpringMvc4Application {
 
--- src/main/java/be/rd/msmvc/ctrl/twitter/UploadCtrl.java
+++ src/main/java/be/rd/msmvc/ctrl/twitter/UploadCtrl.java
@@ -5,9 +5,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URLConnection;
+import java.nio.file.Path;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +19,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import be.rd.msmvc.config.upload.PicturesUploadProperties;
+
 @Controller
 @RequestMapping(path= "/mvc/twitter/profile")
 public class UploadCtrl {
-
-	private static Resource PICTURES_DIR = new FileSystemResource("./pictures");
 	
+	@Autowired
+	private PicturesUploadProperties picturesUploadProperties;
+
 	@RequestMapping(path = "/upload", method = RequestMethod.GET)
 	public String uploadPage() {
 		return "/twitter/profile/uploadPage";
@@ -36,7 +43,9 @@ public class UploadCtrl {
 		
 		String fileName = file.getOriginalFilename();
 		String[] fileInfo = fileName.split("[.]");
-		File tmpFile = File.createTempFile(fileInfo[0], "."+fileInfo[1], PICTURES_DIR.getFile());
+		File tmpFile = File.createTempFile(fileInfo[0], "."+fileInfo[1], 
+				picturesUploadProperties.getUploadPath().getFile());
+		
 		
 		OutputStream out = new FileOutputStream(tmpFile);
 		try(InputStream in = file.getInputStream()){
@@ -46,6 +55,14 @@ public class UploadCtrl {
 		return "/twitter/profile/uploadPage";
 	}
 
+	@RequestMapping(path = "/uploadedPicture")
+	public void getUploadedPicture(HttpServletResponse response) throws IOException {
+		Resource anonymousPcResource = picturesUploadProperties.getAnonymousPicture();
+		response.setHeader("Content-Type",
+				URLConnection.guessContentTypeFromName(anonymousPcResource.getFilename()));
+		IOUtils.copy(anonymousPcResource.getInputStream(), response.getOutputStream());
+	}
+	
 	private boolean isImage(MultipartFile file) {
 		return file.getContentType().startsWith("image");
 	}
