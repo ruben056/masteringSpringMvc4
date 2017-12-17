3b2b9c6c50ee263d24027c3c32e8f967246aef81
ruben056 <demuynck_ruben@hotmail.com>
2017-12-17 11:16:26 +0100

    Added errorhandling and mapping to httpresponsecodes to ctrl * Return responseentity on the ctrl to send default http codes when all is fine * Use custom exceptions and controlleradvice to map those exceptions to the corresponsing http statuscode when all is not fine ...

--- src/main/java/be/rd/msmvc/user/ctrl/RestUserCtrl.java
+++ src/main/java/be/rd/msmvc/user/ctrl/RestUserCtrl.java
@@ -3,12 +3,15 @@ package be.rd.msmvc.user.ctrl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import be.rd.msmvc.general.infrastructure.EntityNotFoundException;
 import be.rd.msmvc.user.dto.User;
 import be.rd.msmvc.user.repository.UserRepository;
 
@@ -25,17 +28,22 @@ public class RestUserCtrl {
 	}
 
 	@RequestMapping(value = "/users", method = RequestMethod.POST)
-	public User createUser(@RequestBody User user) {
-		return userRepository.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		HttpStatus status = (userRepository.exists(user.getEmail())) 
+									? HttpStatus.OK : HttpStatus.CREATED;
+		User saved = userRepository.save(user);
+		return new ResponseEntity<>(saved, status);
 	}
 
 	@RequestMapping(value = "/user/{email}", method = RequestMethod.PUT)
-	public User updateUser(@PathVariable String email, @RequestBody User user) {
-		return userRepository.save(email, user);
+	public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User user) throws EntityNotFoundException{
+		User saved = userRepository.update(email, user);
+		return new ResponseEntity<>(saved, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/user/{email}", method = RequestMethod.DELETE)
-	public void deleteUser(@PathVariable String email) {
+	public ResponseEntity<User> deleteUser(@PathVariable String email) throws EntityNotFoundException {
 		userRepository.delete(email);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- src/main/java/be/rd/msmvc/user/repository/UserRepository.java
+++ src/main/java/be/rd/msmvc/user/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Repository;
 
+import be.rd.msmvc.general.infrastructure.EntityNotFoundException;
 import be.rd.msmvc.user.dto.User;
 
 @Repository
@@ -14,20 +15,35 @@ public class UserRepository {
 
 	private final Map<String, User> userMap = new ConcurrentHashMap<>();
 
-	public User save(String email, User user) {
+	public User update(String email, User user) throws EntityNotFoundException{
+		if(!exists(email)) {
+			throw new EntityNotFoundException("User " + email + " cannot be found");
+		}
 		user.setEmail(email);
 		return userMap.put(email, user);
 	}
 
 	public User save(User user) {
-		return save(user.getEmail(), user);
+		return userMap.put(user.getEmail(), user);
 	}
 
+	public User findOne(String email) throws EntityNotFoundException {
+		if(!exists(email)) {
+			throw new EntityNotFoundException("User " + email + " cannot be found");
+		}
+		User user = userMap.get(email);
+		return user;
+	}
+
+	
 	public List<User> findAll() {
 		return new ArrayList<>(userMap.values());
 	}
 
-	public void delete(String email) {
+	public void delete(String email) throws EntityNotFoundException{
+		if(!exists(email)) {
+			throw new EntityNotFoundException("User " + email + " cannot be found");
+		}
 		userMap.remove(email);
 	}
 
